e9d9b039f96c8c26d58f54ef8e5cf29f5a91da20
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-09-12 08:52:54 -0500

    Holas

--- Lab5Poo/src/main/java/poo/lab5/View/PantallaGaleria.java
+++ Lab5Poo/src/main/java/poo/lab5/View/PantallaGaleria.java
@@ -3,19 +3,24 @@ import poo.lab5.Model.*;
 import poo.lab5.Controller.*;
 public class PantallaGaleria 
 {
-    private ControllerLibro controlLibro=new ControllerLibro();
-    private ControllerAutor controlAutor=new ControllerAutor();
+    private ControllerLibro controlLibro;
+    private ControllerAutor controlAutor;
+    public PantallaGaleria()
+    {
+        this.controlAutor=new ControllerAutor();
+        this.controlLibro=new ControllerLibro();
+    }
     public static void main(String args[]) {
-        PantallaGaleria Galeria = new PantallaGaleria();
         Autor autores[] = new Autor[3];
         autores[0]=new Autor("Nicolas Bayona", 1000000);
         autores[1]=new Autor("Sebastian Herrera", 1000000);
         Libro libro1 = new Libro("Doña barbara", "4EFG", 25000);
         autores[0].setEstado("ACTIVO");
-        Galeria.controlAutor.agregarAutor(autores[0]);
-        Galeria.controlAutor.agregarAutor(autores[1]);
+        ControllerAutor.agregarAutor(autores[0]);
+       
+        ControllerAutor.agregarAutor(autores[1]);
         ControllerLibro.agregarLibro(libro1);
-        Galeria.controlAutor.agregarLibroAAutor(libro1, autores[0]);
+        ControllerAutor.agregarLibroAAutor(libro1, autores[0]);
         for (Autor autor : ControllerAutor.getListaAutores())
         {
             System.out.println(autor);
